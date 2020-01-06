5bb18b20fbfc4cb264b902c1ee864f6af4cf73d0
Vinicius <viniciusdalmazzodecamargo@gmail.com>
2020-01-05 23:55:21 -0300

    Ajustes para exibir tela quando usuário não possui nenhum curso cadastrado no sistema

--- pages/admin/courses/course_list.vue
+++ pages/admin/courses/course_list.vue
@@ -1,156 +1,37 @@
-
 <template>
-  <div>
-    <v-container class="layout">
-      <v-row>
-        <v-col>
-          <v-card height="100%" class="mx-auto">
-            <v-card-text style="height: 100px;" class="grey lighten-5">
-              <h1 class="title-title">Meus Cursos</h1>
+  <v-container class="layout">
+    <v-row>
+      <v-col>
+        <v-card height="100%" class="mx-auto">
+          <v-card-text style="height: 100px;" class="grey lighten-5">
+            <h1 class="title-title">Meus Cursos</h1>
+          </v-card-text>
+          <v-card-text style="position: relative">
+            <v-btn
+              rounded
+              absolute
+              fab
+              dark
+              top
+              right
+              color="#6102b5"
+              class="mx-4 rounded-button"
+            >
+              <v-icon size="50">mdi-plus</v-icon>
+            </v-btn>
+            <v-card-text class="text-center mt-12">
+              <h1 class="no-content-ops">Ops! ):</h1>
             </v-card-text>
-            <v-card-text style="position: relative">
-              <v-btn
-                rounded
-                absolute
-                fab
-                dark
-                top
-                right
-                color="#6102b5"
-                class="mx-4 rounded-button"
-                style="z-index: auto;"
-              >
-                <v-icon size="50">mdi-plus</v-icon>
-              </v-btn>
-
-              <v-card class="mx-auto mt-8" width="100%">
-                <v-container class="pa-0">
-                  <v-row justify="space-between" class="ma-0">
-                    <v-col cols="auto" class="pa-0">
-                      <v-img
-                        height="100%"
-                        width="150"
-                        src="https://i1.wp.com/meprepara.com.br/wp-content/uploads/2017/09/gre-math.jpg?fit=1086%2C724&ssl=1"
-                      ></v-img>
-                    </v-col>
-
-                    <v-col class="pa-0 pt-1 pr-4">
-                      <v-row class="flex-column pl-3 fill-height">
-                        <h3>Matemática do Zero para o Enem</h3>
-                        <p
-                          class="text-sm-left pr-4"
-                          style="word-break: break-word;"
-                        >20 horas de aula com muita teoria e resoluções de exercícios.</p>
-                      </v-row>
-                    </v-col>
-
-                    <v-col cols="auto" class="text-center pa-0">
-                      <v-row class="flex-column ma-0 fill-height" justify="center">
-                        <v-col class="px-0" style="background-color:#6102b5;">
-                          <v-btn fab x-small icon dark>
-                            <v-icon>mdi-pencil</v-icon>
-                          </v-btn>
-                        </v-col>
-
-                        <v-col class="px-0" style="background-color:#E37579;">
-                          <v-btn fab x-small icon dark>
-                            <v-icon>mdi-delete</v-icon>
-                          </v-btn>
-                        </v-col>
-                      </v-row>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card>
-
-              <v-card class="mx-auto mt-8" width="100%">
-                <v-container class="pa-0">
-                  <v-row justify="space-between" class="ma-0">
-                    <v-col cols="auto" class="pa-0">
-                      <v-img
-                        height="100%"
-                        width="150"
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtiNHFSSNG_a6fibOzyCCXVOGG_UTsAyFnrxjZ4WLNnx-u03sE&s"
-                      ></v-img>
-                    </v-col>
-
-                    <v-col class="pa-0 pt-1 pr-4">
-                      <v-row class="flex-column pl-3 fill-height">
-                        <h3>Fotografia para iniciantes</h3>
-                        <p
-                          class="text-sm-left pr-4"
-                          style="word-break: break-word;"
-                        >20 horas de aula com muita teoria e resoluções de exercícios.</p>
-                      </v-row>
-                    </v-col>
-
-                    <v-col cols="auto" class="text-center pa-0">
-                      <v-row class="flex-column ma-0 fill-height" justify="center">
-                        <v-col class="px-0" style="background-color:#6102b5;">
-                          <v-btn fab x-small icon dark>
-                            <v-icon>mdi-pencil</v-icon>
-                          </v-btn>
-                        </v-col>
-
-                        <v-col class="px-0" style="background-color:#E37579;">
-                          <v-btn fab x-small icon dark>
-                            <v-icon>mdi-delete</v-icon>
-                          </v-btn>
-                        </v-col>
-                      </v-row>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card>
-
-              <v-card class="mx-auto mt-8" width="100%">
-                <v-container class="pa-0">
-                  <v-row justify="space-between" class="ma-0">
-                    <v-col cols="auto" class="pa-0">
-                      <v-img
-                        height="100%"
-                        width="150"
-                        src="https://i.ytimg.com/vi/b5KAz_vzwbw/maxresdefault.jpg"
-                      ></v-img>
-                    </v-col>
-
-                    <v-col class="pa-0 pt-1 pr-4">
-                      <v-row class="flex-column pl-3 fill-height">
-                        <h3>Fundamentos do Design Gráfico</h3>
-                        <p
-                          class="text-sm-left pr-4"
-                          style="word-break: break-word;"
-                        >20 horas de aula com muita teoria e resoluções de exercícios.</p>
-                      </v-row>
-                    </v-col>
-
-                    <v-col cols="auto" class="text-center pa-0">
-                      <v-row class="flex-column ma-0 fill-height" justify="center">
-                        <v-col class="px-0" style="background-color:#6102b5;">
-                          <v-btn fab x-small icon dark>
-                            <v-icon>mdi-pencil</v-icon>
-                          </v-btn>
-                        </v-col>
-
-                        <v-col class="px-0" style="background-color:#E37579;">
-                          <v-btn fab x-small icon dark>
-                            <v-icon>mdi-delete</v-icon>
-                          </v-btn>
-                        </v-col>
-                      </v-row>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card>
+            <v-card-text class="text-center">
+              <h2 class="no-content-course">Você não criou nenhum curso</h2>
             </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-      <navigation-bar />
-    </v-container>
-  </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <navigation-bar />
+  </v-container>
 </template>
-
 <router>
 {
     path: '/admin/listar-cursos'
