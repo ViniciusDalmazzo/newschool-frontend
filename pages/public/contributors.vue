87b84221c2c60a489396b76ade2a0b7597da332a
Vinicius <viniciusdalmazzodecamargo@gmail.com>
2020-01-24 23:58:59 -0300

    Criado página que exibe os colaboradores do projeto newschool

---
+++ pages/public/contributors.vue
@@ -0,0 +1,149 @@
+<template>
+  <section>
+    <HeaderBar :title="'CONTRIBUIDORES'" :backPage="true"></HeaderBar>
+    <article id="page">
+      <v-container class="pt-0">
+        <v-layout justify-center>
+          <v-row>
+            <v-col v-for="people in peoples" :key="people.id" cols="12">
+              <v-card raised hover max-width="344" min-width="200" min-height="200">
+                <v-avatar
+                  style="border-radius:50%"
+                  class="mx-auto d-block mt-2"
+                  size="150"
+                >
+                  <img
+                    class="mt-2"
+                    :src="people.image"
+                  />
+                </v-avatar>
+                <v-card-text class="text-center">
+                  <h6 class="category text-gray font-weight-thin mb-2" style="text-transform: uppercase">
+                    {{people.role}}
+                  </h6>
+                  <h4 class="card-title subtitle-1">
+                    {{people.name}}
+                  </h4>                  
+                  <v-btn
+                    class="mt-3"
+                    fab
+                    dark
+                    :href="people.linkedInkUrl" target="_blank"
+                    style="border-radius:50px !important;background:linear-gradient(45deg, #007de0, #00609f, #7ba2da);"
+                  >
+                    <v-icon dark>mdi-linkedin-box</v-icon>
+                  </v-btn>
+                  <v-btn
+                    class="mt-3"
+                    fab
+                    dark
+                    :href="people.instagramLink" target="_blank"
+                    style="border-radius:50px !important;background:linear-gradient(45deg, #405de6, #5851db, #833ab4, #c13584, #e1306c, #fd1d1d);"
+                  >
+                    <v-icon dark>mdi-instagram</v-icon>
+                  </v-btn>
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-layout>
+      </v-container>
+    </article>
+    <navigation-bar />
+  </section>
+</template>
+
+<router>
+  {
+    path: '/contribuidores'
+  }
+</router>
+
+<script>
+import NavigationBar from '~/components/NavigationBar.vue';
+import HeaderBar from '~/components/Header.vue';
+
+export default {
+  components: {
+    HeaderBar,
+    NavigationBar,
+  },
+
+  data() {
+    return {
+      peoples: [
+        {
+          id: 1,
+          image:
+            'https://media-exp2.licdn.com/dms/image/C4E03AQHzZVRHTMYxyg/profile-displayphoto-shrink_800_800/0?e=1585180800&v=beta&t=3Q82hNUf8Tp4cVxku7bAKmkkHonIq7b25GOYvFaEKb0',
+          name: 'Joao Paulo Malara (Jotapê)',
+          role: 'CEO / CO-FOUNDER',
+          linkedInkUrl:
+            'https://www.linkedin.com/in/joao-paulo-malara-jotap%C3%AA-ab9712131',
+          instagramLink:
+            'https://www.instagram.com/joaomalara_',
+        },
+        {
+          id: 2,
+          image:
+            'https://media-exp2.licdn.com/dms/image/C4D03AQFCu5UUnHMB8Q/profile-displayphoto-shrink_800_800/0?e=1585180800&v=beta&t=mCBeTtYAZ4fd6Gnoya9wQPo1-brI-lcO520ITSQ3Qus',
+          name: 'Leonardo Oliveira Balsalobre',
+          role: 'Front-end Developer',
+          linkedInkUrl:
+            'https://www.linkedin.com/in/leonardo-balsalobre/',
+          instagramLink:
+            '',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+::v-deep .title {
+  color: #6600cc;
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 900;
+  font-size: 20px;
+  line-height: 24px;
+}
+::v-deep .descricao {
+  margin-top: 16px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 15px;
+  color: #1a1a1a;
+}
+::v-deep .v-title__title {
+  margin-top: -57px;
+}
+::v-deep .figma .v-image__image {
+  margin-top: -10px;
+}
+::v-deep .figma .v-card__title {
+  margin-top: -33px;
+}
+::v-deep .heroku .v-image__image {
+  height: 70%;
+  margin-top: 7%;
+}
+::v-deep .heroku .v-card__title {
+  margin-top: -20%;
+}
+::v-deep .rollbar .v-image__image {
+  width: 80%;
+  height: 70%;
+  margin: 15% 10%;
+}
+::v-deep #page {
+  max-height: 100vh;
+}
+
+.img-circle {
+  border-radius: 50% !important;
+}
+</style>
